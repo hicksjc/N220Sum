3002c66da0f0bc71fde5bb7b6aa886b19d31834d
hicksjc <jchicks0712@gmail.com>
2021-06-01 11:45:57 -0400

    updated code

--- labs/Assign6/app/app.js
+++ labs/Assign6/app/app.js
@@ -1,18 +1,56 @@
+//Create an animal object with multiple variables inside.
 let myDog = {
     name: "sadie",
-    hunger: 25,
+    hunger: 100,
     happiness: 25,
     age: 11,
     color: "brown",
-
 };
+var counterIncrement= -1;
+var counter = setInterval(HungerLoss(), 1000);
+//variables for HTML stuff
+var DogName = document.getElementById("DogName");
+var DogHunger = document.getElementById("DogHunger");
+var DogHappy = document.getElementById("DogHappy");
 
-console.log(myDog.name);
-function feedSadie(){
-    myDog.hunger = myDog.hunger + 5;
-    console.log(myDog.hunger);
+//writing stuff in HTML
+DogName.innerHTML = "Name: " + myDog.name;
+DogHunger.innerHTML = "Hunger: " + myDog.hunger;
+DogHappy.innerHTML = "Happiness: " + myDog.happiness;
+
+//Create something where when hunger is at 100 the value slowly decreases until it reaches 0.
+//I want to create something that over time decreases the value of hunger and happpiness until it reaches 0.
+//I'm going to have to set a timer of some sort that activates after a few seconds have passed without pressing the button.
+
+//This function executes with the set interval function declared earlier
+function HungerLoss() {
+
+//Subtract by one as long as hunger is above 0
+do {
+    myDog.hunger - 1;
+} while (myDog.hunger != 0);
+
+if (myDog.hunger == 0){
+    alert("Your dog has died!!");
 }
+
+DogHunger.innerHTML = "Hunger: " + myDog.hunger;
+// console.log(myDog.hunger);
+}
+
+DogHunger.innerHTML = "Hunger: " + myDog.hunger;
+//Create a function that increases the value of hunger every time the button is clicked
+function feedSadie(){
+    //Increase hunger by incriment of 5 when the function executes
+    myDog.hunger = myDog.hunger + 5;
+    //display the updated information.
+    DogHunger.innerHTML = "Hunger: " + myDog.hunger;
+
+}
+//Create a function that increases the value of happiness every time the button is clicked
 function playSadie(){
-    myDog.happiness = myDog.happiness + 5;
-    
+    //Increase happiness by incriment of 5 when the function executes.
+    myDog.happiness = myDog.happiness + 5;DogHunger.innerHTML = "Hunger: " + (myDog.hunger - 1);
+    //display the updated information.
+    DogHappy.innerHTML = "Happiness: " + myDog.happiness;
 }
