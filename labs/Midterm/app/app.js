0163a82fa45a80a2333a1668641f7707ff1042e1
hicksjc <jchicks0712@gmail.com>
2021-06-04 18:08:42 -0400

    Updated Code

--- labs/Midterm/app/app.js
+++ labs/Midterm/app/app.js
@@ -5,11 +5,11 @@ function setup(){
     createCanvas(1000,8000);
     //store X and Y cursor location into array, start new circle with size of 20.
     //for loop that executes every 30 frames
-    for(var i = 5; i < 1; i++){
-      //adds a new circle to the array based on mouseX and Y location.
-        circs.push = new circle(mouseX, mouseY, 20);
+    // for(var i = 5; i < 1; i++){
+    //   //adds a new circle to the array based on mouseX and Y location.
+    //     circs.push = new circle(mouseX, mouseY, 20);
         
-    }
+    // }
     console.log(circs);
     // console.log(circs[i]);
 }
@@ -18,13 +18,15 @@ function draw(){
     // circle(circs[0,1,2]);
     // circle(300,200,20);
     //create for loop to draw circles from array, update every 30 frames 
-     for(var i = 0; i < 30; ) {   
+     for(var i = 0; i < 30; i = i+1) {   
         circle(circs[0], circs[1],circs[2] += .1) ;
-        circle(circs[i], circs[i], circs[5] += .1);
+        circle(circs[3], circs[4], circs[5] += .1);
         //increase the circle size by .1 every time
-        circs[3] = circs[3] * .1;
-        //draw the new circle stored in the array.
+        // circs[3] = circs[3] * .1;
+        //Store and draw the new circle in the array
+
+        //draws circle but values stored into array are undefined
     
-        
+        console.log(circs[i]);
     }
 }
