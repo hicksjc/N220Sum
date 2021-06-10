a1e38fefb30d70c1f70a9175b293a191716b0cf9
hicksjc <jchicks0712@gmail.com>
2021-06-10 15:10:05 -0400

    updated code

--- labs/Assign7/app/app.js
+++ labs/Assign7/app/app.js
@@ -1,5 +1,9 @@
+//retrieve the user input
 let txtname = document.getElementById("name");
 
+//greet the user after they type their name and click submit
 function greet() {
+    //greet the user by taking the value of their inputted thing.
+    // the txtname.value refers to the value entered in the textbox in HTML. txtname was the value given to the textbox earlier.
     console.log("Hello, " + txtname.value);
 }
--- labs/Assign7/app/app2.js
+++ labs/Assign7/app/app2.js
@@ -1,8 +1,15 @@
+//Retrieve the username inputbox and assign it to a variable. 
 let username = document.getElementById("username");
+//Retrieve the password inputbox and assign it to a variable.
 let password = document.getElementById("password");
+//retrieve the div that will diplay the result.
 let sumDiv = document.getElementById("check");
 
 function Login(){
+    //Create an if statement where if what is inputted into the username and password textboxes equals Username and Password
+    //it displays as successful
+    //else it displays as wrong information
+    //use .value to retrieve user inputted information within the textboxes.
     if ((username.value == "Username") && (password.value == "Password")){
         sumDiv.innerHTML = "You've Logged in Successfully";
     } else {
