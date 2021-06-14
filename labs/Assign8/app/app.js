5dfeab6c1f5caf71e0ee695296ea59b9a9c3a317
hicksjc <jchicks0712@gmail.com>
2021-06-14 17:20:43 -0400

    Updated Code

--- labs/Assign8/app/app.js
+++ labs/Assign8/app/app.js
@@ -9,9 +9,16 @@ function isDivide() {
     //use an if statement with a %, the % checks for divisible numbers while setting it == 0 takes out any remainders.
     if (numberInput.value % 7 == 0){
         Display.innerHTML = "This number is divisible by 7";
-        return true;
     } else {
         Display.innerHTML = "This number is not divisible by 7";
-        return false;
     }
+    divisible();
+}
+function divisible() {
+    if (numberInput.value % 7 == 0){
+        console.log("true");
+        return true;
+    }
+    console.log("false");
+    return false;
 }
